38b1e4ecb2d5d16d87d244d35f8910d6f1f04a8a
Roman Shevchenko <shevchenko@jet.su>
2021-11-11 13:49:59 -0400

    HW05. without optional. After 1 review

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,20 +1,20 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Query;
 import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
     Meal findByIdAndUserId(int id, int userId);
 
-    List<Meal> findByUserIdOrderByDateTimeDesc(int userId);
+    @Query("select m from Meal m where m.user.id = ?1 order by m.dateTime DESC")
+    List<Meal> getAll(int userId);
 
-    List<Meal> findByUserIdAndDateTimeGreaterThanEqualAndDateTimeLessThanOrderByDateTimeDesc(int userId, LocalDateTime startDateTime, LocalDateTime endDateTime);
+    @Query("select m from Meal m where m.user.id = ?1 and m.dateTime >= ?2 and m.dateTime < ?3 order by m.dateTime DESC")
+    List<Meal> getBetweenHalfOpen(int userId, LocalDateTime startDateTime, LocalDateTime endDateTime);
 
-    @Transactional
     int deleteByIdAndUserId(int id, int userId);
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,12 +1,15 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 @Repository
 public class DataJpaMealRepository implements MealRepository {
 
@@ -19,9 +22,10 @@ public class DataJpaMealRepository implements MealRepository {
     }
 
     @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
-        meal.setUser(crudUserRepository.getById(userId));
         if (meal.isNew() || get(meal.id(), userId) != null) {
+            meal.setUser(crudUserRepository.getById(userId));
             return crudMealRepository.save(meal);
         } else {
             return null;
@@ -29,6 +33,7 @@ public class DataJpaMealRepository implements MealRepository {
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
         return crudMealRepository.deleteByIdAndUserId(id, userId) != 0;
     }
@@ -40,11 +45,11 @@ public class DataJpaMealRepository implements MealRepository {
 
     @Override
     public List<Meal> getAll(int userId) {
-        return crudMealRepository.findByUserIdOrderByDateTimeDesc(userId);
+        return crudMealRepository.getAll(userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return crudMealRepository.findByUserIdAndDateTimeGreaterThanEqualAndDateTimeLessThanOrderByDateTimeDesc(userId, startDateTime, endDateTime);
+        return crudMealRepository.getBetweenHalfOpen(userId, startDateTime, endDateTime);
     }
 }
