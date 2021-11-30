8722c50ff08080fcd6f4f84ceeb51e121a1f1683
Roman Shevchenko <shevchenko@jet.su>
2021-11-30 18:44:57 +0300

    HW07. MealRestController and tests.

---
+++ REST.md
@@ -0,0 +1,25 @@
+#MealRestController
+## getAllMeals
+`curl --location --request GET 'http://localhost:8080/topjava/rest/meals'`
+##getMealsFiltered
+`curl --location --request GET 'http://localhost:8080/topjava/rest/meals/filter?startDate=2020-01-30&startTime=10:00:00&endDate=2020-01-30&endTime=20:00:00'`
+##getMealById
+`curl --location --request GET 'http://localhost:8080/topjava/rest/meals/100007'`
+##createMeal
+`curl --location --request POST 'http://localhost:8080/topjava/rest/meals' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+"dateTime": "2021-01-31T13:00:00",
+"description": "Обед",
+"calories": 1000
+}'`
+##updateMealById
+`curl --location --request PUT 'http://localhost:8080/topjava/rest/meals/100008' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+"dateTime": "2020-11-25T13:00:00",
+"description": "Ужин555",
+"calories": 777
+}'`
+##deleteMealById
+`curl --location --request DELETE 'http://localhost:8080/topjava/rest/meals/100007'`
---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,108 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+import ru.javawebinar.topjava.web.json.JsonUtil;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+import static ru.javawebinar.topjava.UserTestData.user;
+
+class MealRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = MealRestController.REST_URL + '/';
+
+    @Autowired
+    private MealService mealService;
+
+    @Test
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + MEAL1_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MEAL_MATCHER.contentJson(MealTestData.meal1));
+    }
+
+    @Test
+    void getAll() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MEAL_TO_MATCHER.contentJson(
+                        MealsUtil.getTos(MealTestData.meals, user.getCaloriesPerDay())));
+    }
+
+    @Test
+    void delete() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL + MEAL1_ID))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+        assertThrows(NotFoundException.class, () -> mealService.get(MEAL1_ID, USER_ID));
+    }
+
+    @Test
+    void getBetween() throws Exception {
+        LocalDate startDate = LocalDate.of(2020, Month.JANUARY, 30);
+        LocalTime startTime = LocalTime.of(10, 0);
+        LocalDate endDate = LocalDate.of(2020, Month.JANUARY, 30);
+        LocalTime endTime = LocalTime.of(20, 0);
+        List<Meal> mealsDateFiltered = mealService.getBetweenInclusive(startDate, endDate, USER_ID);
+        perform(MockMvcRequestBuilders.get(REST_URL +
+                "filter?startDate=" + startDate +
+                "&startTime=" + startTime +
+                "&endDate=" + endDate +
+                "&endTime=" + endTime))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MEAL_TO_MATCHER.contentJson(
+                        MealsUtil.getFilteredTos(mealsDateFiltered, user.getCaloriesPerDay(), startTime, endTime)
+        ));
+    }
+
+    @Test
+    void createWithLocation() throws Exception {
+        Meal newMeal = MealTestData.getNew();
+        ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(newMeal)))
+                .andExpect(status().isCreated());
+
+        Meal created = MEAL_MATCHER.readFromJson(action);
+        int newId = created.id();
+        newMeal.setId(newId);
+        MEAL_MATCHER.assertMatch(created, newMeal);
+        MEAL_MATCHER.assertMatch(mealService.get(newId, USER_ID), newMeal);
+    }
+
+    @Test
+    void update() throws Exception {
+        Meal updated = MealTestData.getUpdated();
+        perform(MockMvcRequestBuilders.put(REST_URL + MEAL1_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated)))
+                .andExpect(status().isNoContent());
+
+        MEAL_MATCHER.assertMatch(mealService.get(MEAL1_ID, USER_ID), updated);
+    }
+}
